6eec07467e90a9f6ff07682d22428f197045338b
Dhrumil27 <dhrumil.patel.27@gmail.com>
2023-03-23 22:41:51 -0400

    Second commit

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -3,6 +3,10 @@ from decimal import Decimal
 
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+
+def create_user(email='user@example.com', password='testpass123'):
+    """Create a return a new user."""
+    return get_user_model().objects.create_user(email, password)
 class ModelTests(TestCase):
     """Test models."""
 
